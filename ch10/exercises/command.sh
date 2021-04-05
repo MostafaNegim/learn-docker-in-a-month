47b56fe88d5f73142fc2db28dbd829823e873ffd
Mostafa Negim <mostafa.negim@gmail.com>
2021-04-05 08:58:14 +0100

    add: docker-compose.test,uat,dev.yml and command

--- ch10/exercises/command.sh
+++ ch10/exercises/command.sh
@@ -5,3 +5,13 @@ docker-compose -f ./numbers/docker-compose.yml up -d
 docker-compose -f ./todo-list/docker-compose.yml up -d
 # and try another copy of the to-do list:
 docker-compose -f ./todo-list/docker-compose.yml up -d
+
+
+# remove any existing containers
+docker container rm -f $(docker container ls -aq)
+# run the app in dev configuration:
+docker-compose -f ./numbers/docker-compose.yml -f ./numbers/docker-compose-dev.yml -p numbers-dev up -d
+# and the test setup:
+docker-compose -f ./numbers/docker-compose.yml -f ./numbers/docker-compose-test.yml -p numbers-test up -d
+# and UAT:
+docker-compose -f ./numbers/docker-compose.yml -f ./numbers/docker-compose-uat.yml -p numbers-uat up -d
