66b3652307540b70af4887670f91841c559bacb9
Mostafa Negim <mostafa.negim@gmail.com>
2021-04-06 11:29:58 +0100

    update: ch06/command.sh

--- ch06/command.sh
+++ ch06/command.sh
@@ -7,3 +7,7 @@ docker container start --attach f1
 docker container run --name f2 diamol/ch06-file-display
 docker container rm -f f1
 docker container cp f1:/input.txt .
+
+docker container run --name todo1 -d -p 8010:80 diamol/ch06-todo-list
+docker container inspect --format '{{.Mounts}}' todo1
+docker volume ls
