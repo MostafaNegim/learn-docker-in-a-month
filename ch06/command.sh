e2fbbf5d0f0c256765f5946686ff746d00a336db
Mostafa Negim <mostafa.negim@gmail.com>
2021-04-06 11:23:11 +0100

    update: ch06/command.sh

--- ch06/command.sh
+++ ch06/command.sh
@@ -3,3 +3,7 @@ docker run --name f1 diamol/ch06-file-display
 echo "http://eltonstoneman.com" > url.txt
 docker container cp url.txt f1:/input.txt
 docker container start --attach f1
+
+docker container run --name f2 diamol/ch06-file-display
+docker container rm -f f1
+docker container cp f1:/input.txt .
