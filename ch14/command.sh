f079463397b776ad50c6fc0a36bdcba89cedf241
Mostafa Negim <mostafa.negim@gmail.com>
2021-04-13 06:43:39 +0100

    add: v3

--- ch14/command.sh
+++ ch14/command.sh
@@ -6,3 +6,22 @@ docker-compose -f ./numbers/docker-compose.yml -f ./numbers/prod.yml config > st
 docker stack deploy -c stack.yml numbers
 # show the services in the stack:
 docker stack services numbers
+
+
+# join the healthcheck and v2 overrides to the previous files:
+docker-compose -f ./numbers/docker-compose.yml -f ./numbers/prod.yml -f \
+    ./numbers/prod-healthcheck.yml -f ./numbers/v2.yml --log-level \
+    ERROR config > stack.yml
+# update the stack:
+docker stack deploy -c stack.yml numbers
+# check the stack's replicas:
+docker stack ps numbers
+
+
+docker-compose -f ./numbers/docker-compose.yml -f ./numbers/prod.yml \
+    -f ./numbers/prod-healthcheck.yml -f ./numbers/prod-update-config.yml \
+    -f ./numbers/v3.yml --log-level ERROR config > stack.yml
+
+docker stack deploy -c stack.yml numbers
+
+docker stack ps numbers
