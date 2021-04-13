4249fcf0a6f0f2f0026b8c83258ca346fdbfb533
Mostafa Negim <mostafa.negim@gmail.com>
2021-04-13 07:14:39 +0100

    add: prod.rollback-config.yml

--- ch14/command.sh
+++ ch14/command.sh
@@ -25,3 +25,23 @@ docker-compose -f ./numbers/docker-compose.yml -f ./numbers/prod.yml \
 docker stack deploy -c stack.yml numbers
 
 docker stack ps numbers
+
+
+# join lots of Compose files together
+docker-compose -f ./numbers/docker-compose.yml -f ./numbers/prod.yml  \
+    -f ./numbers/prod-healthcheck.yml -f ./numbers/prod-update-config.yml \
+    -f ./numbers/v5-bad.yml config > stack.yml
+# deploy the update:
+docker stack deploy -c stack.yml numbers
+# wait for a minute and check the service status:
+docker service inspect --pretty numbers_numbers-api
+
+
+# join together even more Compose files:
+docker-compose -f ./numbers/docker-compose.yml -f ./numbers/prod.yml -f \
+        ./numbers/prod-healthcheck.yml -f ./numbers/prod-update-config.yml \
+        -f ./numbers/prod-rollback-config.yml -f ./numbers/v5-bad.yml config > stack.yml
+# deploy the update again with the new rollback config:
+docker stack deploy -c stack.yml numbers
+# wait and you'll see it reverts back again:
+docker service inspect --pretty numbers_numbers-api
