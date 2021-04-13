7317ec024a2854e62fa529c7a2a81ccad4c584ce
Mostafa Negim <mostafa.negim@gmail.com>
2021-04-13 04:50:03 +0100

    add: v2

--- ch14/command.sh
+++ ch14/command.sh
@@ -1,3 +1,8 @@
 # cd ch14/exercises
 # join the core Compose file with the production override:
 docker-compose -f ./numbers/docker-compose.yml -f ./numbers/prod.yml config > stack.yml
+
+# deploy the joined Compose file:
+docker stack deploy -c stack.yml numbers
+# show the services in the stack:
+docker stack services numbers
